7300e58af15148cd2dcb1f50fd9f9b73a62f7353
srijeet09 <srijeetsrivastava095@gmail.com>
2023-02-02 15:14:52 +0530

    selection sort

--- bubble.java
+++ bubble.java
@@ -1,24 +1,21 @@
-import java.util.*;
 
-public class bubble {
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++)
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+class selection {
+    public static void selectionSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;
                 }
+            }
+            int sm = arr[index];
+            arr[i] = sm;
         }
-
     }
 
-    public static void main(String[] args) {
-
-        int[] arr1 = { 3, 60, 57, 98 };
-        bubble.bubbleSort(arr1);
-
+    public static void main(String args[]) {
+        int arr1[] = { 1, 2, 3, 4, 5 };
+        selectionSort(arr1);
     }
 }
